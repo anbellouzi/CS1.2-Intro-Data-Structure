3d1b75b1ec97b077a587068617f240b1fcac9f1e
Anas <anbellouzi@gmail.com>
2020-01-23 11:31:46 -0800

    dictionary words

---
+++ dictonary_words.py
@@ -0,0 +1,23 @@
+from random import randint
+
+filename = "/usr/share/dict/words"
+
+
+def returnSentence(lines):
+    index = 5  # how many words to use
+    sentence = ""
+
+    while index > 0:
+        sentence += lines[randint(0, len(lines)-1)].rstrip('\n')
+        sentence += " "
+        index -= 1
+
+    return sentence
+
+if __name__ == '__main__':
+    file = open(filename, 'r')
+    lines = file.readlines()
+
+    sentence = returnSentence(lines)
+
+    print(sentence)
